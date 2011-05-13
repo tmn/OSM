d9ee0adb219f78bf5f576f5598e776751868e4a3
Tri M. Nguyen <trimn@myopera.com>
2011-05-13 04:24:26 +0200

    first upload!

---
+++ Bussholdeplasser/buss.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+use strict;
+use utf8;
+
+use JSON;
+use Data::Dumper;
+
+my $file;
+my $content;
+
+open FILE, "<atb";
+$file = do { local $/; <FILE> };
+close FILE;
+
+$content = from_json($file);
+
+my @stuff = @{ $content->{stops} };
+
+open FILE, ">buss.xml";
+print FILE "<?xml version='1.0' standalone='no'?>\n";
+print FILE "<osm>\n";
+foreach (@stuff) {
+	my $name = $_->{name};
+	$name =~ s/\'/\&apos;/;
+	print FILE "<node id='" . $_->{id} . "' lat='" . $_->{latitude} . "' lon='" . $_->{longitude} . "'>\n";
+	print FILE "<tag k='name' v='" . $name  . "'/>\n";
+	print FILE "</node>\n\n";
+}
+print FILE "</osm>";
+close FILE;
+print "Antall holdeplasser: " . @stuff;
