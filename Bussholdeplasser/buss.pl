64858944cf516edddf44d36a8eb1cad3f6f7690e
Tri M. Nguyen <trimn@myopera.com>
2011-05-14 17:46:22 +0200

    tri goez lazy with commit script and default commit message

--- Bussholdeplasser/buss.pl
+++ Bussholdeplasser/buss.pl
@@ -16,15 +16,15 @@ $content = from_json($file);
 
 my @stuff = @{ $content->{stops} };
 
-open FILE, ">buss.xml";
-print FILE "<?xml version='1.0' standalone='no'?>\n";
-print FILE "<osm>\n";
+open FILE, ">buss_minified.xml";
+print FILE "<?xml version='1.0' standalone='no'?>";
+print FILE "<osm>";
 foreach (@stuff) {
 	my $name = $_->{name};
 	$name =~ s/\'/\&apos;/;
-	print FILE "<node id='" . $_->{id} . "' lat='" . $_->{latitude} . "' lon='" . $_->{longitude} . "'>\n";
-	print FILE "<tag k='name' v='" . $name  . "'/>\n";
-	print FILE "</node>\n\n";
+	print FILE "<node lat='" . $_->{latitude} . "' lon='" . $_->{longitude} . "'>";
+	print FILE "<tag k='name' v='" . $name  . "'/>";
+	print FILE "</node>";
 }
 print FILE "</osm>";
 close FILE;
